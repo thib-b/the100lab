8ac2f2169bf37f6d5b2b9f26fc9cff03b31484f2
Thibault <thib@Thibaults-Mac-mini.local>
2026-09-11 20:40:58 +0100

    content: reorder nav sections
    Change the section order from Video / Bio / Tour / Image / Store / Contact to
    Tour / Store / Bio / Video / Image / Contact.

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,8 +1,8 @@
 export const NAV = [
-  { title: 'video', url: '/video/' },
-  { title: 'bio', url: '/bio/' },
   { title: 'tour', url: '/tour/' },
-  { title: 'image', url: '/image/' },
   { title: 'store', url: 'https://robocobraquartet.bandcamp.com/merch', external: true },
+  { title: 'bio', url: '/bio/' },
+  { title: 'video', url: '/video/' },
+  { title: 'image', url: '/image/' },
   { title: 'contact', url: '/contact/' },
 ];
